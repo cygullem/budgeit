7e5f24dbf952f449ebaedbb30faa59bde3a876d7
Cy Gullem <crmchs.gullem.clydecyril@gmail.com>
2025-08-09 22:37:01 +0800

    refactor: minor adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function LandingPage() {
             className="pointer-events-none fixed inset-0 z-0"
             style={{
                backgroundImage: `
-            radial-gradient(circle at 30% 70%, rgba(59, 130, 246, 0.6), transparent 70%),
-            radial-gradient(circle at 70% 30%, rgba(236, 72, 153, 0.7), transparent 70%)
-         `,
+                  radial-gradient(circle at 30% 70%, rgba(59, 130, 246, 0.6), transparent 70%),
+                  radial-gradient(circle at 70% 30%, rgba(236, 72, 153, 0.7), transparent 70%)
+               `,
                backgroundAttachment: 'fixed',
                backgroundPosition: 'center',
                backgroundRepeat: 'no-repeat',
